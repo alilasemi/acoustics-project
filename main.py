fe7e295d88986e357c6acaa7aacca96e1c1ffda4
alilasemi <alasemi@stanford.edu>
2022-06-02 14:00:27 -0700

    Plot for many n_r and n_theta

--- main.py
+++ main.py
@@ -4,56 +4,89 @@ import scipy, scipy.special
 import numpy as np
 import sympy as sp
 
+# -- Inputs -- #
+gamma = 1.4     # Specific heat ratio (given for air)
+R = 287         # Gas constant (given for air)
+radius = 1      # Radius of tube
+xL = 0          # Position of left end of tube
+xR = 1          # Position of right end of tube
+p = 20          # Polynomial order of spectral method
+max_n_r = 9     # Radial mode number
+max_n_theta = 9 # Angular mode number
+nb = p + 1      # Number of basis functions
+
 def main():
-
-    # Physics inputs
-    gamma = 1.4     # Specific heat ratio (given for air)
-    R = 287         # Gas constant (given for air)
-    radius = 1      # Radius of tube
-    n_r = 1         # Radial mode number
-    n_theta = 1     # Angular mode number
-    # Numerics inputs
-    xL = 0
-    xR = 1
-    p = 200
-    use_chebyshev_nodes = True
-
-    # -- Compute lambda -- #
-    # j_prime actually has a root at x = 0 for n_theta != 1, but this is not
-    # included in the Scipy function, so consider this case separately
-    if n_theta != 1 and n_r == 0:
-        j_prime = 0
-    # This situation is not valid, since J1 does not have a root at 0
-    if n_theta == 1 and n_r == 0:
-        print('J1 does not have a root at 0!')
-    # Otherwise, the Scipy function is used
-    else:
-        j_prime = scipy.special.jnp_zeros(n_theta, n_r)[0]
-    lambd = j_prime / radius
 
     # Number of basis functions
     nb = p + 1
-    # Create grid of collocation points. Option between using Chebyshev nodes
-    # (the roots of the p + 1 order Chebyshev polynomial) or using equispaced
-    # points (not recommended).
+    # Create grid of collocation points. Use Chebyshev nodes
+    # (the roots of the p + 1 order Chebyshev polynomial).
     num_points = nb
-    if use_chebyshev_nodes:
-        x = np.polynomial.chebyshev.Chebyshev([0]*(nb) + [1], domain=[xL, xR]).roots()
-    else:
-        x = np.linspace(xL, xR, num_points)
+    x = np.polynomial.chebyshev.Chebyshev([0]*(nb) + [1], domain=[xL, xR]).roots()
 
     # Some function for temperature
     xs = sp.Symbol('x', real=True)
     # Options: constant vs. Gaussian bump
     # TODO
-    T0_expr = 50 * sp.exp(-200 * (xs - .1)**2) + 300
-    #T0_expr = 300 + .00000000001 * xs**2
+    #T0_expr = 50 * sp.exp(-200 * (xs - .1)**2) + 300
+    T0_expr = 300 + .00000000001 * xs**2
 
     T0_func = sp.lambdify(xs, T0_expr)
     T0 = T0_func(x).reshape(-1, 1)
     dT0_dx_expr = T0_expr.diff(xs)
     dT0_dx_func = sp.lambdify(xs, dT0_dx_expr)
     dT0_dx = dT0_dx_func(x).reshape(-1, 1)
+
+    cutoff = np.empty((max_n_r + 1, max_n_theta + 1))
+    # Loop over the different radial mode numbers
+    for n_r in range(max_n_r + 1):
+        # Loop over the different angular mode numbers
+        for n_theta in range(max_n_theta + 1):
+            # Compute the cutoff frequency
+            cutoff[n_r, n_theta] = compute_cutoff_frequency(x, T0, dT0_dx, n_r, n_theta, p)
+
+    # -- Plot --  #
+    rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+    rc('text', usetex=True)
+    # Plot the base state temperature profile
+    fig = plt.figure(figsize=(5, 5))
+    plt.plot(x, T0, 'k--', linewidth=3, label='$T_0$')
+    plt.xlabel('$x$', fontsize=20)
+    plt.ylabel('$T_0$', fontsize=20)
+    plt.tick_params(labelsize=16)
+    plt.legend(loc='best', fontsize = 20)
+    plt.grid(linestyle='--')
+    plt.tight_layout()
+    plt.savefig('T0.pdf', bbox_inches='tight')
+    # Plot the cutoff frequencies for different n_r, n_theta
+    fig = plt.figure(figsize=(5, 5))
+    for n_theta in range(max_n_theta + 1):
+        plt.plot(range(max_n_r + 1), cutoff[:, n_theta], '.-', ms=10,
+                label=f'$n_\\theta = {n_theta}$')
+    plt.xlabel('$n_r$', fontsize=20)
+    plt.ylabel('$\\omega_\\textrm{cutoff}$ (rad/s)', fontsize=20)
+    plt.tick_params(labelsize=16)
+    plt.legend(loc='best', fontsize=11, ncol=2)
+    plt.grid(linestyle='--')
+    plt.tight_layout()
+    plt.savefig('result.pdf', bbox_inches='tight')
+    plt.show()
+
+
+def compute_cutoff_frequency(x, T0, dT0_dx, n_r, n_theta, p):
+    # -- Compute lambda -- #
+    # j_prime actually has a root at x = 0 for n_theta != 1, but this is not
+    # included in the Scipy function, so consider this case separately
+    if n_theta != 1 and n_r == 0:
+        j_prime = 0
+    # This situation is not valid, since J1 does not have a root at 0
+    elif n_theta == 1 and n_r == 0:
+        print('J1 does not have a root at 0!')
+        return np.nan
+    # Otherwise, the Scipy function is used
+    else:
+        j_prime = scipy.special.jnp_zeros(n_theta, n_r)[-1]
+    lambd = j_prime / radius
 
     # Matrix to be solved
     A = np.empty((nb, nb))
@@ -104,42 +137,15 @@ def main():
 
     # Get eigenvalues and right eigenvectors (in columns)
     eigvals, eigvecs = scipy.linalg.eig(A, phi_RHS)
-    omega2 = np.sort(eigvals)
-    omega = np.sqrt(omega2)
-    cut_on = omega[0]
-    breakpoint()
-
-    ## Dense output points
-    #num_pts = 100
-    #xd = np.linspace(xL, xR, num_pts)
-    #X = np.polynomial.chebyshev.Chebyshev(xhat[:, 0], domain=[xL, xR])(xd)
-
-    # Plot
-    rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-    rc('text', usetex=False)
-
-    fig = plt.figure(figsize=(5, 5))
-    plt.plot(x, T0, 'k--', linewidth=3, label='$T_0$')
-    plt.xlabel('$x$', fontsize=20)
-    plt.ylabel('$T_0$', fontsize=20)
-    plt.tick_params(labelsize=16)
-    plt.legend(loc='best', fontsize = 20)
-    plt.grid(linestyle='--')
-    plt.tight_layout()
-    plt.savefig('T0.pdf', bbox_inches='tight')
-
-    fig = plt.figure(figsize=(5, 5))
-    #plt.plot(xd, X, 'k', linewidth=3, label='$X$')
-    #plt.plot(x, T0, 'k--', linewidth=3, label='$T_0$')
-    plt.plot(np.real(omega), np.imag(omega), 'k.', ms=7, label='$T_0$')
-    plt.xlabel('$x$', fontsize=20)
-    plt.ylabel('$X$', fontsize=20)
-    plt.tick_params(labelsize=16)
-    plt.legend(loc='best', fontsize = 20)
-    plt.grid(linestyle='--')
-    plt.tight_layout()
-    plt.savefig('result.pdf', bbox_inches='tight')
-    plt.show()
+    # Specifically, get the eigenvalues which have no (very little) imaginary
+    # part. The imaginary values are caused by numerical error in the scheme.
+    real_eigvals = np.real(eigvals[np.abs(np.imag(eigvals)) < .1])
+    # Also toss out negatives, and the number 0. Then, take the square root to
+    # get omega, and sort smallest to largest.
+    omega = np.sort(np.sqrt(real_eigvals[real_eigvals > 0.0001]))
+    # Take the first one to be the cutoff frequency
+    cutoff = omega[0]
+    return cutoff
 
 if __name__ == '__main__':
     main()
