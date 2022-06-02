d7abd72ea1c515dff1b75b35cb9707d53ad70d83
alilasemi <alasemi@stanford.edu>
2022-06-02 15:19:05 -0700

    Plot for different bump temperatures

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ import numpy as np
 import sympy as sp
 
 # -- Inputs -- #
-gamma = 1.4     # Specific heat ratio (given for air)
-R = 287         # Gas constant (given for air)
-radius = 1      # Radius of tube
-xL = 0          # Position of left end of tube
-xR = 1          # Position of right end of tube
-p = 20          # Polynomial order of spectral method
-max_n_r = 9     # Radial mode number
-max_n_theta = 9 # Angular mode number
-nb = p + 1      # Number of basis functions
+gamma = 1.4         # Specific heat ratio (given for air)
+R = 287             # Gas constant (given for air)
+radius = 1          # Radius of tube
+xL = 0              # Position of left end of tube
+xR = 1              # Position of right end of tube
+p = 40              # Polynomial order of spectral method
+max_n_r = 5         # Radial mode number
+max_n_theta = 5     # Angular mode number
+nb = p + 1          # Number of basis functions
+T_ambient = 300     # Temperature of ambient fluid
+T_bump_max = 4000   # Maximum temperature of the peak of the Gaussian bump
+n_T = 20            # Number of temperatures between 0 and T_bump_max
 
 def main():
 
@@ -24,52 +27,79 @@ def main():
     num_points = nb
     x = np.polynomial.chebyshev.Chebyshev([0]*(nb) + [1], domain=[xL, xR]).roots()
 
-    # Some function for temperature
-    xs = sp.Symbol('x', real=True)
-    # Options: constant vs. Gaussian bump
-    # TODO
-    #T0_expr = 50 * sp.exp(-200 * (xs - .1)**2) + 300
-    T0_expr = 300 + .00000000001 * xs**2
+    # Possible values of the temperature peak
+    T_bump = np.linspace(T_ambient, T_bump_max, n_T)
+    T0_list = []
+    dT0_dx_list = []
+    cutoff_list = []
+    min_cutoff = np.empty(n_T)
+    # Loop over each temprature peak
+    for i in range(n_T):
+        # Gaussian bump function for temperature
+        xs = sp.Symbol('x', real=True)
+        T0_expr = (T_bump[i] - T_ambient + 1e-12) * sp.exp(-100 * (xs - .1)**2) + T_ambient
+        # Create function and evaluate at points
+        T0_func = sp.lambdify(xs, T0_expr)
+        T0_list.append(T0_func(x).reshape(-1, 1))
+        # Compute the derivative of the profile symbolically
+        dT0_dx_expr = T0_expr.diff(xs)
+        # Evaluate at points
+        dT0_dx_func = sp.lambdify(xs, dT0_dx_expr)
+        dT0_dx_list.append(dT0_dx_func(x).reshape(-1, 1))
 
-    T0_func = sp.lambdify(xs, T0_expr)
-    T0 = T0_func(x).reshape(-1, 1)
-    dT0_dx_expr = T0_expr.diff(xs)
-    dT0_dx_func = sp.lambdify(xs, dT0_dx_expr)
-    dT0_dx = dT0_dx_func(x).reshape(-1, 1)
-
-    cutoff = np.empty((max_n_r + 1, max_n_theta + 1))
-    # Loop over the different radial mode numbers
-    for n_r in range(max_n_r + 1):
-        # Loop over the different angular mode numbers
-        for n_theta in range(max_n_theta + 1):
-            # Compute the cutoff frequency
-            cutoff[n_r, n_theta] = compute_cutoff_frequency(x, T0, dT0_dx, n_r, n_theta, p)
+        cutoff = np.empty((max_n_r + 1, max_n_theta + 1))
+        # Loop over the different radial mode numbers
+        for n_r in range(max_n_r + 1):
+            # Loop over the different angular mode numbers
+            for n_theta in range(max_n_theta + 1):
+                # Compute the cutoff frequency
+                cutoff[n_r, n_theta] = compute_cutoff_frequency(x, T0_list[-1],
+                        dT0_dx_list[-1], n_r, n_theta, p)
+        cutoff_list.append(cutoff)
+        # Store the minimum cutoff frequency
+        min_cutoff[i] = np.nanmin(cutoff)
 
     # -- Plot --  #
     rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
     rc('text', usetex=True)
-    # Plot the base state temperature profile
+    # Plot the base state temperature profile for the lowest and highest
+    # temperature cases
     fig = plt.figure(figsize=(5, 5))
-    plt.plot(x, T0, 'k--', linewidth=3, label='$T_0$')
+    plt.plot(x, T0_list[0], 'k--', linewidth=3, label=f'$T_0$, Ambient')
+    plt.plot(x, T0_list[-1], 'k', linewidth=3, label=f'$T_0$, Max Heating')
     plt.xlabel('$x$', fontsize=20)
     plt.ylabel('$T_0$', fontsize=20)
     plt.tick_params(labelsize=16)
     plt.legend(loc='best', fontsize = 20)
     plt.grid(linestyle='--')
     plt.tight_layout()
-    plt.savefig('T0.pdf', bbox_inches='tight')
-    # Plot the cutoff frequencies for different n_r, n_theta
+    plt.savefig(f'T0.pdf', bbox_inches='tight')
+    # Plot the cutoff frequencies for different n_r, n_theta for the lowest and
+    # highest temperature cases
+    for i in [0, n_T - 1]:
+        fig = plt.figure(figsize=(5, 5))
+        for n_theta in range(max_n_theta + 1):
+            plt.plot(range(max_n_r + 1), cutoff_list[i][:, n_theta], '.-', ms=10,
+                    label=f'$n_\\theta = {n_theta}$')
+        plt.xlabel('$n_r$', fontsize=20)
+        plt.ylabel('$\\omega_\\textrm{cutoff}$ (Hz)', fontsize=20)
+        plt.xlim([-.2, 5.2])
+        plt.ylim([0, 1300])
+        plt.tick_params(labelsize=16)
+        plt.legend(loc='best', fontsize=12, ncol=2)
+        plt.grid(linestyle='--')
+        plt.tight_layout()
+        plt.savefig(f'cutoff_{int(T_bump[i])}.pdf', bbox_inches='tight')
+    # Plot the minimum cutoff frequency for each temperature case
     fig = plt.figure(figsize=(5, 5))
-    for n_theta in range(max_n_theta + 1):
-        plt.plot(range(max_n_r + 1), cutoff[:, n_theta], '.-', ms=10,
-                label=f'$n_\\theta = {n_theta}$')
-    plt.xlabel('$n_r$', fontsize=20)
-    plt.ylabel('$\\omega_\\textrm{cutoff}$ (rad/s)', fontsize=20)
+    plt.plot(T_bump, min_cutoff, 'k', linewidth=3, label=None)
+    plt.xlabel('$T_0$', fontsize=20)
+    plt.ylabel('$\\omega_\\textrm{cutoff}$ (Hz)', fontsize=20)
     plt.tick_params(labelsize=16)
-    plt.legend(loc='best', fontsize=11, ncol=2)
+    #plt.legend(loc='best', fontsize = 20)
     plt.grid(linestyle='--')
     plt.tight_layout()
-    plt.savefig('result.pdf', bbox_inches='tight')
+    plt.savefig(f'min_cutoff.pdf', bbox_inches='tight')
     plt.show()
 
 
@@ -145,7 +175,8 @@ def compute_cutoff_frequency(x, T0, dT0_dx, n_r, n_theta, p):
     omega = np.sort(np.sqrt(real_eigvals[real_eigvals > 0.0001]))
     # Take the first one to be the cutoff frequency
     cutoff = omega[0]
-    return cutoff
+    # Convert rad/s to Hz
+    return cutoff / (2 * np.pi)
 
 if __name__ == '__main__':
     main()
